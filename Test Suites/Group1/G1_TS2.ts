d873e18c866747310e9ad585132d5231c64eca6b
nhacle <nguyennhac2904@gmail.com>
2018-09-18 17:17:00 +0700

    commit from user1

---
+++ Test Suites/Group1/G1_TS2.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>G1_TS2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-09-18T17:15:38</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>17801ff1-0b49-4bdc-a296-89987a3b0051</testSuiteGuid>
+   <testCaseLink>
+      <guid>59e3389c-33b6-461a-9601-fe14147dfd48</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Group1/G1_TS3.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>G1_TS3</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-09-18T17:13:50</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>895f340f-c5a3-4617-aba2-859503a95e66</testSuiteGuid>
+   <testCaseLink>
+      <guid>81f545bc-14b4-4097-894f-5133a030e2c0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Logout</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>21d1118f-7b4b-4b39-8c7b-b3078a9a1653</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
